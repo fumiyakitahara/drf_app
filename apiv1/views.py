5647ff2a1e85dd4665b7ed4eee31a519953f43de
fumiyasaitou <saitofumiyaxx@gmail.com>
2022-12-02 01:33:32 +0900

    modelとserializer追加

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from shop.models import Book
+from .serializers import BooSerializer
 
 # Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    """本モデルのCRUD用APIクラス"""
+
+    queryset = Book.objects.all()
+    serializer_class = BooSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
